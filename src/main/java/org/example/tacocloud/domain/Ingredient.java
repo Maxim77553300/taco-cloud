a0c2e472d09bc0a51544dcb55afb70fa2ed2a6b7
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2024-08-24 11:16:24 +0300

    add Controllers and templates

---
+++ src/main/java/org/example/tacocloud/domain/Ingredient.java
@@ -0,0 +1,21 @@
+package org.example.tacocloud.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@EqualsAndHashCode
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}

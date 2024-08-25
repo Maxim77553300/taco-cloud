cb17b2f677b6188fd5ebf5d8c3841ead1f00ce66
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2024-08-25 10:00:48 +0300

    review

--- src/main/java/org/example/tacocloud/controller/DesignTacoController.java
+++ src/main/java/org/example/tacocloud/controller/DesignTacoController.java
@@ -8,8 +8,12 @@ import org.example.tacocloud.domain.Taco;
 import org.example.tacocloud.domain.TacoOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.Arrays;
 import java.util.List;
